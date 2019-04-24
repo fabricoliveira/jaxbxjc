dcf8dcbeb441076fee88f49cb3a5bc7cf5c22078
fabricoliveira <fabricoliveira@192.168.0.100>
2019-04-24 00:12:33 -0300

    first commit

---
+++ src/main/java/com/estudo/jaxb/jaxbxjc/App.java
@@ -0,0 +1,13 @@
+package com.estudo.jaxb.jaxbxjc;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
