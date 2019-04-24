d26bce8a5e5af54c175f904ba8b4b295a85dd841
fabricoliveira <fabricoliveira@192.168.0.100>
2019-04-24 00:17:47 -0300

    removing /src/generated folder

--- src/generated/com/tempuri/patient/package-info.java
+++
@@ -1,9 +0,0 @@
-//
-// Este arquivo foi gerado pela Arquitetura JavaTM para Implementação de Referência (JAXB) de Bind XML, v2.3.0 
-// Consulte <a href="https://javaee.github.io/jaxb-v2/">https://javaee.github.io/jaxb-v2/</a> 
-// Todas as modificações neste arquivo serão perdidas após a recompilação do esquema de origem. 
-// Gerado em: 2019.04.23 às 10:45:36 PM BRT 
-//
-
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://www.tempuri.com/Patient", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package com.tempuri.patient;
